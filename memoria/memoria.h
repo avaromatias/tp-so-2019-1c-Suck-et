60f76583c0ce64129d1466055769abf37d928548
GastiV <gnvidal25@gmail.com>
2019-04-18 13:41:07 -0300

    Agrego instancia globar de t_config para memoria

--- memoria/memoria.h
+++ memoria/memoria.h
@@ -19,6 +19,7 @@
 #include <commons/log.h>
 
 #include "../libs/config.h"
+#include "../libs/sockets.h"
 
 typedef struct {
     int puerto;
@@ -35,6 +36,8 @@ typedef struct {
 } t_configuracion;
 
 t_configuracion cargarConfiguracion(char* path, t_log* logger);
+
+t_configuracion configuracion; //Declaro mi instancia de t_configuracion como global
 
 
 #endif /* MEMORIA_H_ */

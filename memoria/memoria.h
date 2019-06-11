64baa17f8b5b4f5e2cc1aafb416a111c22ea4796
GastiV <gnvidal25@gmail.com>
2019-06-10 23:40:31 -0300

    Journal prepara las querys a enviar a lissandra

--- memoria/memoria.h
+++ memoria/memoria.h
@@ -107,6 +107,13 @@ void eliminarSegmento(void* segmento);
 
 //JOURNAL
 //void enviarInsertLissandra(t_control_conexion* conexionConLissandra, char* key, char* value, char* timestamp);
-void enviarInsertLissandra(char* key, char* value, char* timestamp);
-char* nombreSegmento;
+typedef  struct {
+    char* nombreTabla;
+    t_control_conexion* conexionLissandra;
+    t_log* logger;
+
+}parametros_journal;
+void mi_dictionary_iterator(parametros_journal* parametrosJournal, t_dictionary *self, void(*closure)(parametros_journal*,char*,void*));
+void enviarInsertLissandra(parametros_journal* parametrosJournal, char* key, char* value, char* timestamp);
+
 #endif /* MEMORIA_H_ */
